26fccaec8c92e39b2cc123782bac5c086789e5f7
dhakad-speed <dhakadtryspeed@gmail.com>
2025-09-16 15:58:59 +0530

    allDone

--- app/layout.js
+++ app/layout.js
@@ -3,16 +3,6 @@ import Navbar from "@/component/Navbar";
 import Footer from "@/component/Footer";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Home | PortFolio",
   description: "Home Page",
@@ -25,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-[#1f1f1f] dark:text-[#ffffffa3] bg-white text-[#0c0407]`}
+        className={`antialiased dark:bg-[#1f1f1f] dark:text-[#ffffffa3] bg-white text-[#0c0407]`}
       >
         <Navbar />
         {children}
